ec4b42eee07a7ccf2f073e01d8a880501176f254
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-10 16:49:41 +0700

    Init code

---
+++ main.tf
@@ -0,0 +1,68 @@
+resource "azurerm_resource_group" "k8s_rg" {
+  name = var.resource_group_name
+  location = var.location
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_network_interface" "master_nic" {
+  name = "${var.vm_name}-master-nic"
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  location = var.location
+  internal_dns_name_label = "${var.vm_name}-master"
+  enable_accelerated_networking = var.enable_accelerated_networking
+
+  ip_configuration {
+    name = "${var.vm_name}-master-ipconf"
+    subnet_id = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_virtual_machine" "master-vm" {
+  name = "${var.vm_name}-master-nic"
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  location = var.location
+  network_interface_ids = [azurerm_network_interface.master_nic.id]
+  vm_size = var.vm_size
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    id = var.os_image_id
+    publisher = var.os_image_id == "" ? var.os_image_publisher : ""
+    offer = var.os_image_id == "" ? var.os_image_offer : ""
+    sku = var.os_image_id == "" ? var.os_image_sku : ""
+    version = var.os_image_id == "" ? var.os_image_version : ""
+  }
+
+  storage_os_disk {
+    name = "${var.vm_name}-master-osdisk"
+    create_option = "FromImage"
+    caching = "ReadWrite"
+    managed_disk_type = var.os_disk_type
+  }
+
+  os_profile {
+    computer_name = "${var.vm_name}-master"
+    admin_username = var.admin_username
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path = "/home/${var.admin_username}/.ssh/authorized_keys"
+      key_data = var.ssh_public_key
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [tags,storage_image_reference]
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,64 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+}
+
+variable "location" {
+  description = "Location of resources"
+}
+
+variable "vm_name" {
+  description = "VM name"
+}
+
+variable "enable_accelerated_networking" {
+  description = "Enable accelerated networking or not"
+  type = bool
+  default = false
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the VM"
+}
+
+variable "private_ip_address_allocation" {
+  description = "Type of address allocation"
+  default = "Dynamic"
+}
+
+variable "vm_size" {
+  description = "Default size of the VM"
+  default = "Standard_B2s"
+}
+
+variable "os_image_publisher" {
+  description = "Publisher of the OS image"
+  default = "Canonical"
+}
+
+variable "os_image_offer" {
+  description = "Offer of the OS image"
+  default = "0001-com-ubuntu-server-focal"
+}
+
+variable "os_image_sku" {
+  description = "SKU of the OS image"
+  default = "20_04-lts-gen2"
+}
+
+variable "os_image_version" {
+  description = "Version of the OS image"
+  default = "latest"
+}
+
+variable "os_disk_type" {
+  description = "Defines the type of storage account to be created. Valid options are Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS."
+  default     = "Standard_LRS"
+}
+
+variable "admin_username" {
+  description = "Admin username of the VM"
+}
+
+variable "ssh_public_key" {
+  description = "Public key for SSH"
+}
